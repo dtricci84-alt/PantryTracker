0ca325f274aaf12331936a98f9c2a6382706d616
dtricci84-alt <dtricci84@gmail.com>
2026-08-24 11:20:26 +0200

    Bump PantryTracker to v1.6

--- PantryTrackerV1_3/PantryTrackerV1_3/app/build.gradle.kts
+++ PantryTrackerV1_3/PantryTrackerV1_3/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.pantrytracker"
         minSdk = 26
         targetSdk = 37
-        versionCode = 6
-        versionName = "1.5"
+        versionCode = 7
+        versionName = "1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
